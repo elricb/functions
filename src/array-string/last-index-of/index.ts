8bd856e638206256ae4891a5bda310a35b60364d
Elric Best <spiritofelric@gmail.com>
2022-08-03 13:00:38 -0400

    Added array-string last-index-of, find maximum index of matches found

---
+++ src/array-string/last-index-of/index.ts
@@ -0,0 +1,16 @@
+// position after last string found
+export default function (s: string, a: Array<string>): number {
+  let last = -1;
+
+  for (let i = 0; i < a.length; i++) {
+    let j = s.lastIndexOf(a[i]);
+
+    if (j !== -1) {
+      j = j + a[i].length;
+      last = j > last ? j : last;
+    }
+  }
+
+  return last;
+}
+
