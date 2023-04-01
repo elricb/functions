86738f43286ef0c28143b7a928ac65f0ea11605e
Elric Best <spiritofelric@gmail.com>
2023-03-31 20:49:45 -0400

    Fixed - jest switched to commonjs and babel

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  roots: ["src"],
-  preset: "ts-jest",
+  roots: ["<rootDir>/src"],
+  //preset: 'ts-jest',
   testEnvironment: "node"
 };
